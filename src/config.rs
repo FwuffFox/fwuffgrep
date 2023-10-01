2079307c1e52bee5e95c18293488aba9333d609c
FwuffFox <FwuffFox@gmail.com>
2023-10-01 19:43:30 +0300

    FwuffGrep 1.0.0

--- src/config.rs
+++ src/config.rs
@@ -1,25 +1,24 @@
 use std::path::PathBuf;
-
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(author, version, about, long_about = None)]
+#[command(author = "Fwuff", version = "1.0.0", about = "Basic implementation of a grep command written in rust", long_about = None)]
 pub struct Config {
     /// Query to search by
     pub query: String,
 
-    /// Path to a file. If none, read from stdin
+    /// Path to a file to read from. If none, input is read from stdin
     pub file_path: Option<PathBuf>,
 
-    /// Ignore case
+    /// Ignore case. Doesn't work if using Regex search
     #[arg(short, long, action)]
-    pub case_insensitive: bool,
+    pub ignore_case: bool,
 
-    /// Use regex search
+    /// Use regex search. Overrides an ignore case flag
     #[arg(short, long, action)]
     pub regex: bool,
 
-    /// Path to output file
-    #[arg(short, long)]
+    /// Path to output file. If none, text is written to a stdout
+    #[arg(short , long)]
     pub output_path: Option<PathBuf>,
 }
---
+++ tests/search_tests.rs
@@ -0,0 +1,37 @@
+#[cfg(test)]
+mod search_tests {
+    use fwuffgrep::{*, input::input_lines_from_string};
+
+    #[test]
+    fn test_search_case_sensitive() {
+        let query = "Line";
+        let test_input = "Line 1\nAnother line\nYet Another Line\n";
+        let lines = input_lines_from_string(test_input.to_owned());
+
+        let results = search(query, lines, false).unwrap();
+
+        assert_eq!(results, vec![(1, "Line 1".to_string()), (3, "Yet Another Line".to_string())]);
+    }
+
+    #[test]
+    fn test_search_case_insensitive() {
+        let query = "line";
+        let test_input = "Line 1\nAnother Line\nYet Another line\n";
+        let lines = input_lines_from_string(test_input.to_owned());
+
+        let results = search(query, lines, true).unwrap();
+
+        assert_eq!(results, vec![(1, "Line 1".to_string()), (2, "Another Line".to_string()), (3, "Yet Another line".to_string())]);
+    }
+
+    #[test]
+    fn test_search_no_results() {
+        let query = "MatchThis";
+        let test_input = "Line 1\nAnother Line\nYet Another line\n";
+        let lines = input_lines_from_string(test_input.to_owned());
+
+        let results = search(query, lines, true).unwrap();
+
+        assert_eq!(results, Vec::<(usize, String)>::new());
+    }
+}
